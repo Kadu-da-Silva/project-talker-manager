e9e153fdf3ad9db79d4d4f51bce209d9f3ce3f2f
Carlos Eduardo <carlao0010@hotmail.com>
2023-07-20 19:54:51 -0300

    Req 04 Ok

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const crypto = require('crypto');
-
 const app = express();
 app.use(express.json());
 
+const crypto = require('crypto');
 const talkers = require('./talker.json');
+// const validEmail = require('./middlewares/validEmail.json');
 
 app.get('/talker', (req, res) => {
   res.status(200).json(talkers);
@@ -33,7 +33,33 @@ function generateRandomToken(length) {
 //   password: '123456',
 // };
 
-app.post('/login', (req, res) => {
+const validEmail = (req, res, next) => {
+  const { email } = req.body;
+  // Se o campo não estiver preenchido responde mensagem de erro
+  if (!email) res.status(400).json({ message: 'O campo \"email\" é obrigatório' })
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const emailValid = emailRegex.test(email);
+  if (emailValid) {
+    // Se o campo email estiver preenchido corretamente
+    return next();
+  } else {
+    res.status(400).json({ message: 'O \"email\" deve ter o formato \"email@email.com\"' });
+  }
+};
+
+const validPassword = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) res.status(400).json({ message: 'O campo \"password\" é obrigatório'})
+
+  if (password.length >= 6) {
+    return next();
+  } else {
+    res.status(400).json({ message: 'O \"password\" deve ter pelo menos 6 caracteres'})
+  }
+};
+
+app.post('/login', validEmail, validPassword, (req, res) => {
   const token = generateRandomToken(16);
   res.status(200).json({ token: `${token}` });
 
