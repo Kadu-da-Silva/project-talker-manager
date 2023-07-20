2e2dd70c320739dc8967947f153e155b353a3a3f
Carlos Eduardo <carlao0010@hotmail.com>
2023-07-20 20:12:04 -0300

    Refatorando Index

--- src/index.js
+++ src/index.js
@@ -8,9 +8,13 @@ const talkers = require('./talker.json');
 const validEmail = require('./middlewares/validEmail');
 const validPassword = require('./middlewares/validPassword');
 
+// 1 - Crie o endpoint GET /talker
+
 app.get('/talker', (req, res) => {
   res.status(200).json(talkers);
 });
+
+// 2 - Crie o endpoint GET /talker/:id
 
 app.get('/talker/:id', (req, res) => {
   const { id } = req.params;
@@ -21,6 +25,9 @@ app.get('/talker/:id', (req, res) => {
     res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
 });
+
+// 3 - Crie o endpoint POST /login
+// 4 - Adicione as validações para o endpoint /login
 
 // Função para gerar um token aleatório
 function generateRandomToken(length) {
